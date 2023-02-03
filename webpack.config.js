159ee5593f94dd9599094a55a9938b30f77d88ac
mohan8git <bishnoimohan502@gmail.com>
2023-02-03 19:59:04 +0530

    added webpack

--- webpack.config.js
+++ webpack.config.js
@@ -15,18 +15,18 @@ module.exports = {
   mode: 'development',
   output: {
     path: path.resolve(__dirname, './dist'),
-    publicPath: '/_assets/',
+    publicPath: '/',
     filename: 'index_bundle.js',
   },
   target: 'web',
   devServer: {
     port: '6001',
     static: {
-      publicPath: '/_assets/',
+      publicPath: '/',
       directory: path.join(__dirname, 'static'),
     },
     historyApiFallback: {
-      rewrites: [{ from: /^\/.*$/, to: '/_assets/index.html' }],
+      rewrites: [{ from: /^\/.*$/, to: '/index.html' }],
     },
     open: true,
     hot: true,
